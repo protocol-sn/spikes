5d33525eb9c6b0f3ab73be64a955eae274d42535
John <patrick.meyerin@gmail.com>
2025-08-15 12:57:01 -0500

    missed some unneeded logging

--- kubernetes-ecosystem/backend-one/src/main/java/coop/stlma/protocolsn/spike/KeycloakRoleFinder.java
+++ kubernetes-ecosystem/backend-one/src/main/java/coop/stlma/protocolsn/spike/KeycloakRoleFinder.java
@@ -17,15 +17,12 @@ public class KeycloakRoleFinder implements RolesFinder {
 
     @Override
     public List<String> resolveRoles(Map<String, Object> attributes) {
-        System.out.println("attributes: " + attributes);
         //We don't necessarily know what types of Objects micronaut will render the attribute as, so this should be
         //implemented in a safer way. That is outside the scope of this spike and will be future John's problem.
         Map<String, Object> realmAccess = (Map<String, Object>) attributes.get("realm_access");
         if (realmAccess == null) {
             return Collections.emptyList();
         } else {
-            log.info("Found roles: {}", realmAccess.get("roles"));
-            System.out.println("Found roles: " + realmAccess.get("roles"));
             return (List<String>) realmAccess.get("roles");
         }
     }
